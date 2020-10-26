31ff45dd75ad0e115855f97b5f3c6a484bc7ba63
ritwiksaraf <35697880+ritwiksaraf@users.noreply.github.com>
2020-10-26 11:13:39 +0530

    Update Upload.php

--- Admin/Upload.php
+++ Admin/Upload.php
@@ -86,7 +86,6 @@ if (empty($username)){
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <script src="../Includes/scripts/script.js"></script>
 <script src="../Includes/scripts/overlay.js"></script>
-<script src="/Includes/scripts/file-upload.js"></script>
 </html>
 
 <?php
