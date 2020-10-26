7583e04a75c360fa697a8c2e6f3a4160f26af1b2
ritwiksaraf <35697880+ritwiksaraf@users.noreply.github.com>
2020-10-26 10:50:57 +0530

    Update dashboard.php

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -1,15 +1,4 @@
-<?php
 
-include '../Database/connect.php';
-session_start();
-
-$username = $_SESSION['user'];
-
-if (empty($username)){
-	header("Location: index.php");
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -90,3 +79,15 @@ while ($row = $result->fetchArray()){ ?>
     </body>
 </html>
 
+<?php
+
+include '../Database/connect.php';
+session_start();
+
+$username = $_SESSION['user'];
+
+if (empty($username)){
+	header("Location: index.php");
+}
+
+?>
