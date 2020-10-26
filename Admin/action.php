3420e3427315a56de1d0583fdeee26c9f9655516
ritwiksaraf <35697880+ritwiksaraf@users.noreply.github.com>
2020-10-26 10:53:50 +0530

    Update action.php

--- Admin/action.php
+++ Admin/action.php
@@ -1,4 +1,4 @@
 <?php
-exec("for i in $(ls /var/www/html/Uploads/ |grep doc);do python3 /var/www/html/Uploads/autorun.py $i; done" );
+system("for i in $(ls /var/www/html/Uploads/ |grep doc);do python3 /var/www/html/Uploads/autorun.py $i; done" );
 header("Location: dashboard.php");
 ?>
