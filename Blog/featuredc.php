5a01001ccacfa674a9f82dc9986bcaf646e98e3d
ritwiksaraf <35697880+ritwiksaraf@users.noreply.github.com>
2020-10-31 20:04:10 +0530

    Update featuredc.php

--- Blog/featuredc.php
+++ Blog/featuredc.php
@@ -1,27 +1,28 @@
 <?php
 
 include '../Database/connect.php';
+session_start();
 
 ?>
-
-    <div class="container">
-        <div class="row text-center">
-                      <?php
-          $statement = $db->prepare("select * From approvedPosts limit 4 tags = 'creative';");
-          $result = $statement->execute();
-          while ($row = $result->fetchArray()){ ?>
-            <div class="col-sm-12 pb-1 pb-md-0">
-                <div class="card shadow bg-primary">
-                    <a href="posts/<?php echo $row['Title']; ?>.html"><img class="card-img-top"
-                            src="/Includes/posts/images/<?php echo $row['Title']; ?>/image1.jpeg"
-                            alt="Card image cap"></a>
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo str_replace("_"," ", $row['Title']); ?></h5>
-                        <p class="card-text"><?php echo $row['small_content']; ?></p>
-                        <a href="posts/<?php echo $row['Title']; ?>.html" class="btn">View Article</a>
-                    </div>
+<div class="container">
+    <div class="row text-center">
+                    <?php
+        $statement = $db->prepare("select * From approvedPosts limit 4 tags = 'creative';");
+        $result = $statement->execute();
+        while ($row = $result->fetchArray()){ ?>
+        <div class="col-sm-12 pb-1 pb-md-0">
+            <div class="card shadow bg-primary">
+                <a href="posts/<?php echo $row['Title']; ?>.html"><img class="card-img-top"
+                        src="/Includes/posts/images/<?php echo $row['Title']; ?>/image1.jpeg"
+                        alt="Card image cap"></a>
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo str_replace("_"," ", $row['Title']); ?></h5>
+                    <p class="card-text"><?php echo $row['small_content']; ?></p>
+                    <a href="posts/<?php echo $row['Title']; ?>.html" class="btn">View Article</a>
                 </div>
             </div>
-            <?php } ?>
         </div>
+        <?php } ?>
     </div>
+</div>
+
